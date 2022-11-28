762c60b9b63da7f216ea879ace4ea6a082a14037
Anil Altunkan <mehmet_anil_altunkan@homedepot.com>
2022-11-28 00:10:06 -0600

    initial commit

---
+++ custom-vars.el
@@ -0,0 +1,27 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(auto-save-interval 0)
+ '(auto-save-list-file-prefix nil)
+ '(auto-save-timeout 0)
+ '(auto-show-mode t t)
+ '(delete-auto-save-files nil)
+ '(delete-old-versions 'other)
+ '(history-length t)
+ '(imenu-auto-rescan t)
+ '(imenu-auto-rescan-maxout 500000)
+ '(kept-new-versions 5)
+ '(kept-old-versions 5)
+ '(make-backup-file-name-function 'ignore)
+ '(make-backup-files nil)
+ '(package-selected-packages
+   '(yasnippet origami-predef yaml-mode go-mode json-mode gnu-elpa-keyring-update ggtags ##)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
